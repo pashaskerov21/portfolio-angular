8a355902a8afd05c78c9629e8d21f721c39061fd
pasha <askerovpasha21@gmail.com>
2025-09-23 20:56:47 +0400

    experience

--- src/app/sections/experience/experience.ts
+++ src/app/sections/experience/experience.ts
@@ -1,11 +1,32 @@
 import { Component } from '@angular/core';
+import { PersonalInformationDataType } from '../../../types';
+import { ApiService } from '../../api/api';
+import { NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-experience',
-  imports: [],
+  imports: [NgFor],
   templateUrl: './experience.html',
   styleUrl: './experience.scss'
 })
 export class Experience {
+  personalData: PersonalInformationDataType | null = null;
 
+  constructor(private apiService: ApiService) { }
+  ngOnInit(): void {
+    // datalarin sehife yuklenende fetch edilmesi
+    this.loadInformation();
+  }
+  // personal datanin fetch edilmesi funksiyasi
+  loadInformation() {
+    this.apiService.getInformation().subscribe({
+      next: (data) => {
+        this.personalData = data || null
+      },
+      error: (err) => {
+        console.log('Information fetch failed', err);
+        this.personalData = null;
+      }
+    })
+  }
 }
