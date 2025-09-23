8d87228e1a05de9e10a3259487ab8a9c7e9c4a4d
pasha <askerovpasha21@gmail.com>
2025-09-24 01:48:56 +0400

    skills

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -3,10 +3,11 @@ import { About } from "../../sections/about/about";
 import { Banner } from "../../sections/banner/banner";
 import { Services } from "../../sections/services/services";
 import { Experience } from "../../sections/experience/experience";
+import { Skills } from "../../sections/skills/skills";
 
 @Component({
   selector: 'app-home',
-  imports: [About, Banner, Services, Experience],
+  imports: [About, Banner, Services, Experience, Skills],
   templateUrl: './home.html',
   styleUrl: './home.scss'
 })
--- src/app/sections/skills/skills.ts
+++ src/app/sections/skills/skills.ts
@@ -1,11 +1,32 @@
 import { Component } from '@angular/core';
+import { SkillDataType } from '../../../types';
+import { ApiService } from '../../api/api';
+import { NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-skills',
-  imports: [],
+  imports: [NgFor],
   templateUrl: './skills.html',
   styleUrl: './skills.scss'
 })
 export class Skills {
+  skillsData: SkillDataType[] = [];
 
+  constructor(private apiService: ApiService) { }
+  ngOnInit(): void {
+    // datalarin sehife yuklenende fetch edilmesi
+    this.loadSkills();
+  }
+  // skill datanin fetch edilmesi funksiyasi
+  loadSkills() {
+    this.apiService.getSkills().subscribe({
+      next: (data) => {
+        this.skillsData = data || []
+      },
+      error: (err) => {
+        console.log('Information fetch failed', err);
+        this.skillsData = [];
+      }
+    })
+  }
 }
